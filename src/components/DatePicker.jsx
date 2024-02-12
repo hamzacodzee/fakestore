8fd0278aa889c6ca59e8f350cebccfb006b085eb
hamzacodzee <hamza.codzee@gmail.com>
2024-02-12 15:57:33 +0530

    datepicker

--- src/components/DatePicker.jsx
+++ src/components/DatePicker.jsx
@@ -1,44 +1,93 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 
-const DatePicker = () => {
-  const [dob, setDob] = useState("");
-  const [today, setToday] = useState("");
-
-  useEffect(() => {
-    const dob2 = new Date(dob);
-    const today2 = new Date(today);
-    const diff = today2 - dob2;
-
-    const temp = Math.floor(diff / 1000 / 60 / 60 / 24 / 30 / 12);
-   
-  }, [dob, today]);
-
+const DatePicker = ({ selectedDate, onDateChange, label }) => {
   return (
     <div>
-      <h1>Age Finder</h1>
+      <label htmlFor={label}>{label}:</label>
       <input
         type="date"
-        id="dob"
-        name="dob"
-        onChange={(e) => {
-          setDob(e.target.value);
-        }}
+        id={label.toLowerCase()}
+        name={label.toLowerCase()}
+        value={selectedDate?.toISOString().split("T")[0]}
+        onChange={(e) => onDateChange(e.target.value)}
+        max={new Date().toISOString().split("T")[0]}
       />
-      <br />
-
-      <input
-        type="date"
-        id="today"
-        name="today"
-        onChange={(e) => {
-          setToday(e.target.value);
-        }}
-      />
-      <br />
-      <h1>DOB: {dob}</h1>
-      <h1>Today: {today}</h1>
     </div>
   );
 };
 
-export default DatePicker;
+const AgeCalculator = () => {
+  const [dob, setDob] = useState(new Date());
+  const [today, setToday] = useState(new Date());
+  const [age, setAge] = useState({
+    years: 0,
+    months: 0,
+    days: 0,
+  });
+
+  const handleDateChange = (value, setDate) => {
+    const selectedDate = new Date(value);
+
+    if (selectedDate > today) {
+      alert("Selected date should be less than today");
+    } else {
+      setDate(selectedDate);
+      const diff = today - selectedDate;
+      const years = diff / (1000 * 60 * 60 * 24 * 365.25);
+      const months = (years - Math?.floor(years)) * 12;
+      const days = (months - Math?.floor(months)) * (365.25 / 12);
+
+      setAge({
+        years: Math?.floor(years),
+        months: Math?.floor(months),
+        days: Math?.floor(days),
+      });
+    }
+  };
+
+  const handleDateChangeToday = (value, setDate) => {
+    const selectedDate = new Date(value);
+
+    if (selectedDate < dob) {
+      alert("Selected date should be more than dob");
+    } else {
+      setToday(selectedDate);
+      const diff = selectedDate - dob;
+      const years = diff / (1000 * 60 * 60 * 24 * 365);
+      const months = (years - Math?.floor(years)) * 12;
+      const days = (months - Math?.floor(months)) * (365 / 12);
+
+      setAge({
+        years: Math?.floor(years),
+        months: Math?.floor(months),
+        days: Math?.floor(days),
+      });
+    }
+  };
+
+  return (
+    <div>
+      <h1>Age Finder</h1>
+      <DatePicker
+        selectedDate={dob}
+        onDateChange={(value) => handleDateChange(value, setDob)}
+        label="Date of Birth"
+      />
+      <DatePicker
+        selectedDate={today}
+        onDateChange={(value) => handleDateChangeToday(value, setToday)}
+        label="Today"
+      />
+      <br />
+
+      <h4>DOB: {dob?.toLocaleDateString()}</h4>
+      <h4>Today: {today?.toLocaleDateString()}</h4>
+
+      <h3>
+        Age: {age?.years} Years, {age?.months} Months, {age?.days} Days
+      </h3>
+    </div>
+  );
+};
+
+export default AgeCalculator;
