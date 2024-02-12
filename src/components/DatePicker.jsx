f8c30065638922ec2732a28e6c782661ab528e38
hamzacodzee <hamza.codzee@gmail.com>
2024-02-12 17:27:32 +0530

    fialdatepicker

--- src/components/DatePicker.jsx
+++ src/components/DatePicker.jsx
@@ -1,87 +1,95 @@
 import React, { useState } from "react";
 
-const DatePicker = ({ selectedDate, onDateChange, label }) => {
-  return (
-    <div>
-      <label htmlFor={label}>{label}:</label>
-      <input
-        type="date"
-        id={label.toLowerCase()}
-        name={label.toLowerCase()}
-        value={selectedDate?.toISOString().split("T")[0]}
-        onChange={(e) => onDateChange(e.target.value)}
-        max={new Date().toISOString().split("T")[0]}
-      />
-    </div>
-  );
-};
-
 const AgeCalculator = () => {
-  const [dob, setDob] = useState(new Date());
-  const [today, setToday] = useState(new Date());
+  const [dob, setDob] = useState(null);
+  const [dod, setDod] = useState(null);
+
   const [age, setAge] = useState({
     years: 0,
     months: 0,
     days: 0,
   });
 
+  const calAge = (years, months, days) => {
+    setAge({
+      years: Math?.floor(years),
+      months: Math?.floor(months),
+      days: Math?.floor(days),
+    });
+  };
+
   const handleDateChange = (value, setDate) => {
     const selectedDate = new Date(value);
 
-    if (selectedDate > today) {
-      alert("Selected date should be less than today");
-    } else {
-      setDate(selectedDate);
-      const diff = today - selectedDate;
-      const years = diff / (1000 * 60 * 60 * 24 * 365.25);
-      const months = (years - Math?.floor(years)) * 12;
-      const days = (months - Math?.floor(months)) * (365.25 / 12);
-
-      setAge({
-        years: Math?.floor(years),
-        months: Math?.floor(months),
-        days: Math?.floor(days),
-      });
+    setDate(selectedDate);
+    const diff = dod - selectedDate;
+    const years = diff / (1000 * 60 * 60 * 24 * 365);
+    const months = (years - Math?.floor(years)) * 12;
+    const days = (months - Math?.floor(months)) * (365 / 12);
+    if (dod != null) {
+      calAge(years, months, days);
     }
   };
 
-  const handleDateChangeToday = (value, setDate) => {
+  const handleDateChangeDod = (value, setDate) => {
     const selectedDate = new Date(value);
 
-    if (selectedDate < dob) {
-      alert("Selected date should be more than dob");
-    } else {
-      setToday(selectedDate);
-      const diff = selectedDate - dob;
-      const years = diff / (1000 * 60 * 60 * 24 * 365);
-      const months = (years - Math?.floor(years)) * 12;
-      const days = (months - Math?.floor(months)) * (365 / 12);
-
-      setAge({
-        years: Math?.floor(years),
-        months: Math?.floor(months),
-        days: Math?.floor(days),
-      });
+    setDod(selectedDate);
+    const diff = selectedDate - dob;
+    const years = diff / (1000 * 60 * 60 * 24 * 365);
+    const months = (years - Math?.floor(years)) * 12;
+    const days = (months - Math?.floor(months)) * (365 / 12);
+    if (dob != null) {
+      calAge(years, months, days);
     }
   };
 
   return (
     <div>
       <h1>Age Finder</h1>
-      <DatePicker
-        selectedDate={dob}
-        onDateChange={(value) => handleDateChange(value, setDob)}
-        label="Date of Birth"
-      />
-      <DatePicker
-        selectedDate={today}
-        onDateChange={(value) => handleDateChangeToday(value, setToday)}
-        label="Today"
-      />
+      <div>
+        <label htmlFor="dob">dob:</label>
+        <input
+          type="date"
+          id="dob"
+          name="dob"
+          value={dob ? dob?.toISOString().split("T")[0] : ""}
+          onChange={(e) => handleDateChange(e.target.value, setDob)}
+          {...(dod
+            ? {
+                max: new Date(dod.getTime() - 24 * 60 * 60 * 1000)
+                  .toISOString()
+                  .split("T")[0],
+              }
+            : {
+                max: new Date(new Date().getTime() - 24 * 60 * 60 * 1000)
+                  .toISOString()
+                  .split("T")[0],
+              })}
+        />
+      </div>
       <br />
 
-      <h4>DOB: {dob?.toLocaleDateString()}</h4>
-      <h4>Today: {today?.toLocaleDateString()}</h4>
+      <div>
+        <label htmlFor="dod">dod:</label>
+        <input
+          type="date"
+          id="dod"
+          name="dod"
+          value={dod ? dod?.toISOString().split("T")[0] : ""}
+          onChange={(e) => handleDateChangeDod(e.target.value, setDod)}
+          max={new Date().toISOString().split("T")[0]}
+          {...(dob
+            ? {
+                min: new Date(dob.getTime() + 24 * 60 * 60 * 1000)
+                  .toISOString()
+                  .split("T")[0],
+              }
+            : {})}
+        />
+      </div>
+
+      <br />
 
       <h3>
         Age: {age?.years} Years, {age?.months} Months, {age?.days} Days
